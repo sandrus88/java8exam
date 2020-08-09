d7fa8931d41fac3a9ee28f359267cf4c96c30e7a
Sandro <sandrus88@hotmail.it>
2020-08-09 11:43:03 +0200

    formatting class + ignore file

--- src/exam/TestClass.java
+++ src/exam/TestClass.java
@@ -2,26 +2,26 @@ package exam;
 
 import exam.exceptions.MyException;
 
-public class TestClass{
-    public static void main(String[] args) {
-        try{
-            hello();
-        }
-        catch(MyException me){
-            System.out.println(me);
-        }
-    }
-    
-    static void hello() throws MyException{
-        int[] dear = new int[7];
-        dear[0] = 747;
-        foo();
-    }
+public class TestClass {
 
-    /**
-     * This method throws an Exception
-     */
-    static void foo() throws MyException{
-        throw new MyException("Exception from foo");
-    }
+	public static void main(String[] args) {
+		try {
+			hello();
+		} catch (MyException me) {
+			System.out.println(me);
+		}
+	}
+
+	static void hello() throws MyException {
+		int[] dear = new int[7];
+		dear[0] = 747;
+		foo();
+	}
+
+	/**
+	 * This method throws an Exception
+	 */
+	static void foo() throws MyException {
+		throw new MyException("Exception from foo");
+	}
 }
