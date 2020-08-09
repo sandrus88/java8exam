10681f3121ac45801436be862b22af436dae48c6
Sandro <sandrus88@hotmail.it>
2020-08-09 13:03:32 +0200

    creato classe personale MyClass4.

---
+++ src/exam/test1/MyClass4.java
@@ -0,0 +1,22 @@
+package exam.test1;
+
+/**
+ * Consider the following code for the main() method:
+   What will be the output when the above code is executed?
+     i=6 j=6
+   + i=5 j=6
+     i=5 j=5
+     i=6 j=5
+     None of these. 
+ */
+
+public class MyClass4{
+
+	public static void main(String[] args) throws Exception{
+	   int i = 1, j = 10;
+	   do {
+	      if (i++ > --j) continue;
+	   } while (i < 5);
+	   System.out.println("i=" + i + " j=" + j);
+	}
+}
