58149dd24eaa3bb6ee094bf1c4430d12999caab2
Sandro <sandrus88@hotmail.it>
2020-08-12 17:06:08 +0200

    risoluzione esercizio10 con creazione del main().

---
+++ src/exam/test1/ClassX.java
@@ -0,0 +1,18 @@
+package exam.test1;
+
+/**
+ 
+ Which of the following are valid at line 1? 
+ String s; ( ok )
+ String s = 'asdf';
+ String s = 'a';
+ String s = this.toString(); ( ok in un metodo non statico )
+ String s = asdf;
+ 
+ */
+
+public class ClassX {
+	public static void main(String args[]) {
+		//line 1: insert code here.
+		}
+}
