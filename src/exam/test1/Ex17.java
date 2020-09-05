9787a10185eeaa82522c0d06d7ed314e9db6ff61
Sandro <sandrus88@hotmail.it>
2020-09-05 11:04:56 +0200

    correzione testo Ex17 (come si dice in linguaggio tecnico? :) che non me lo ricordo

--- src/exam/test1/Ex17.java
+++ src/exam/test1/Ex17.java
@@ -1,14 +1,24 @@
 package exam.test1;
 
 /**
- * Consider the following method... public int setVar(int a, int b, float c)
- * {...} Which of the following methods correctly overload the above method?
+ * Consider the following method... public int setVar(int a, int b, float c){...}
+ * Which of the following methods correctly overload the above method?
  * 
- * 1) public int setVar(int a, float b, int c){    return (int)(a + b + c); };
- * 2) public int setVar(int a, float b, int c){    return this(a, c, b); }; 3)
- * public int setVar(int x, int y, float z){    return x+y; }; 4) public float
- * setVar(int a, int b, float c){    return c*a; }; 5) public float setVar(int
- * a){    return a; };
+ * 1) public int setVar(int a, float b, int c){    
+ *    return (int)(a + b + c); 
+ *    };
+ * 2) public int setVar(int a, float b, int c){    
+ *    return this(a, c, b); 
+ *    }; 
+ * 3) public int setVar(int x, int y, float z){    
+ *    return x+y; 
+ *    }; 
+ * 4) public float setVar(int a, int b, float c){    
+ *    return c*a; 
+ *    }; 
+ * 5) public float setVar(int a){    
+ *    return a; 
+ *    };
  */
 public class Ex17 {
 	public int setVar(int a, int b, float c) {
