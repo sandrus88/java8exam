7a72c1e9eb4fc58746881cda529d122cb754945e
Ermal <ermal.aliraj@gmail.com>
2020-09-10 12:03:48 +0200

    ex17 - overloading examples (conflicts)

--- src/exam/test1/Ex17.java
+++ src/exam/test1/Ex17.java
@@ -1,16 +1,6 @@
 package exam.test1;
 
 /**
-<<<<<<< HEAD
- * Consider the following method... public int setVar(int a, int b, float
- * c){...} Which of the following methods correctly overload the above method?
- * 
- * 1) public int setVar(int a, float b, int c){    return (int)(a + b + c); };
- * 2) public int setVar(int a, float b, int c){    return this(a, c, b); }; 3)
- * public int setVar(int x, int y, float z){    return x+y; }; 4) public float
- * setVar(int a, int b, float c){    return c*a; }; 5) public float setVar(int
- * a){    return a; };
-=======
  * Consider the following method... public int setVar(int a, int b, float c){...}
  * Which of the following methods correctly overload the above method?
  * <p>
@@ -29,57 +19,8 @@ package exam.test1;
  * 5) public float setVar(int a){ ï¿½ï¿½
  * return a;
  * };
-<<<<<<< HEAD
->>>>>>> refs/remotes/origin/master
-=======
->>>>>>> branch 'master' of https://github.com/sandrus88/java8exam.git
  */
 public class Ex17 {
-<<<<<<< HEAD
-
-<<<<<<< HEAD
-	public int setVar(int a, float b, int c) {
-		return (int) (a + b + c);
-	}
-=======
->>>>>>> branch 'master' of https://github.com/sandrus88/java8exam.git
-
-<<<<<<< HEAD
-	public int setVar(int a, float b, int c) {    
-		return this(a, c, b); 
-=======
-    //who from the followings overloads the original methods?
-    // https://www.programiz.com/java-programming/method-overloading
-    public int setVar(int a, float b, int c) { //different argument sequence, same return type
-        return (int) (a + b + c);
-    }
-    public int setVar(int a, float b, int c) {  //different argument sequence, same return type
-        return this(a, c, b); //when this is used?  is this correct?
-    }
-    public int setVar(int x, int y, float z) { //this has the same argument sequence, same return type. can we say is an overloading case?
-        return x + y;
-    }
-    public float setVar(int a, int b, float c) { //this has same argument sequence, but different return type. is the case of overloading?
-        return c * a;
-    }
-    public float setVar(int a) { //different argument, but different return type too. is teh case of overloading?
-        return a;
->>>>>>> branch 'master' of https://github.com/sandrus88/java8exam.git
-    }
-
-<<<<<<< HEAD
-	public int setVar(int x, int y, float z) {    
-		return x+y; 
-	}
-
-	public float setVar(int a, int b, float c) {    
-		return c*a; 
-	}
-	
-	public float setVar(int a) {    
-		return a; 
-	}
-=======
     //who from the followings overloads the original methods?
     // https://www.programiz.com/java-programming/method-overloading
     public int setVar(int a, float b, int c) { //different argument sequence, same return type
@@ -97,10 +38,13 @@ public class Ex17 {
     public float setVar(int a) { //different argument, but different return type too. is teh case of overloading?
         return a;
     }
->>>>>>> refs/remotes/origin/master
 
-<<<<<<< HEAD
-	public static void main(String[] args) {
+    //original methods
+    public int setVar(int a, int b, float c) {
+        return a;
+    }
+
+    public static void main(String[] args) {
 		Ex17 ex = new Ex17();
 		System.out.println(ex.setVar(1, 2, (float)2.5));
 		System.out.println(ex.setVar(1, (float)4.5, 5));
@@ -109,26 +53,4 @@ public class Ex17 {
 		System.out.println(ex.setVar(1, 3, (float)5.5));
 		System.out.println(ex.setVar(1));
 	}
-=======
-    //original methods
-    public int setVar(int a, int b, float c) {
-        return a;
-    }
-
-    public static void main(String[] args) {
-        Ex17 ex = new Ex17();
-        System.out.println(ex.setVar());
-    }
->>>>>>> refs/remotes/origin/master
-=======
-    //original methods
-    public int setVar(int a, int b, float c) {
-        return a;
-    }
-
-    public static void main(String[] args) {
-        Ex17 ex = new Ex17();
-        System.out.println(ex.setVar());
-    }
->>>>>>> branch 'master' of https://github.com/sandrus88/java8exam.git
 }
