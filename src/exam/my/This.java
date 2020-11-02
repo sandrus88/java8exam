08b235a9699313405377376ee70b3979e8c7d22d
Ermal <ermal.aliraj@gmail.com>
2020-11-02 15:07:13 +0100

    ex17 - overloading examples (conflicts)

--- src/exam/my/This.java
+++ src/exam/my/This.java
@@ -1,13 +1,40 @@
 package exam.my;
 
 public class This {
+    
+    private int a;
+    private int b;
+    
+    public This() {
+        this(0, 0);
+    }
+    
+    public This(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    public void setVar(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    
+    // Esercizio: crea un metodo resetVar che ressetta tutti i valori allo stato inziale.
 
-	public int setVar(int a, float b, int c) {  
-		return this(a, c, b); 
+    // ese2. crea un toString dove vengono printate i valori.
+    public String toString() {
+        String objectState = "[a: " + a;
+        return objectState;
     }
 
-	public static void main(String[] args) {
-		This ex = new This();
-		System.out.println(ex.setVar(1, (float)2.5, 5));
+    public static void main(String[] args) {
+        This ex1 = new This();
+        System.out.println("ex1 fase 1: " + ex1);
+    
+        ex1.setVar(4, 7);
+        System.out.println("ex1 fase 1: " + ex1);
+    
+        This ex2 = new This(2, 2);
+        System.out.println("ex2 fase 1: " + ex2);
     }
 }
